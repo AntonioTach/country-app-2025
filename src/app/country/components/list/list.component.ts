b7270a4a70b34c715a49b077ece70c835855f07b
AntonioTach <antach.vihe@gmail.com>
2025-08-09 23:47:02 -0600

    FEAT: Implement country data handling with service, mapper, and interface definitions

--- src/app/country/components/list/list.component.ts
+++ src/app/country/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'country-list',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListComponent { }
+export class ListComponent {
+  countries = input.required<Country[]>()
+}
--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { ListComponent } from "../../components/list/list.component";
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,8 +10,25 @@ import { ListComponent } from "../../components/list/list.component";
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+
+  isLoading = signal<boolean>(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
 
   onSearch(capital: string) {
-    console.log('Searching for capital:', capital);
+    if (this.isLoading()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(capital).subscribe(
+      (countries) => {
+        this.isLoading.set(false);
+        this.countries.set(countries);
+        console.log(countries);
+
+      }
+    )
   }
 }
